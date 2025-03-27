7574a17e063c90388b2de33447da484708034320
Janhavi Pawar <pawarjanhavi80@gmail.com>
2025-03-27 19:00:38 +0530

    updated project

---
+++ src/app/AgentComponents/register-car/register-car.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-register-car',
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './register-car.component.html',
+  styleUrl: './register-car.component.css'
+})
+export class RegisterCarComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './HomePageComponents/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './HomePageComponents/footer/footer.component';
 
 @Component({
   selector: 'app-root',
